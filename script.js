8c11cd4caf6139d46aa0d8e737272f51eccf809a
LJAMINE <aminelmgrmj@gmai.com>
2024-11-08 00:04:08 +0100

    4th button plus et moins

--- script.js
+++ script.js
@@ -52,5 +52,86 @@ function scrollPage(sectionId) {
    
  
 
+
+   let  plusValeurAdult=document.querySelector(".plusbuttonAdult")
+    let moinsValeurAdult=document.querySelector(".minusbuttonAdult")
+    let numbeerAdult=document.querySelector('#numbeerAdult')
+    let  plusValeurMoins=document.querySelector(".plusbuttonSmall")
+    let moinsValeurMoins=document.querySelector(".moinsbuttonSmall")
+    let numbeerMoins=document.querySelector('#numbeerSmall')
+
+
+    // adult
+
+
+let i=0;
+    plusValeurAdult.addEventListener("click",function(){
+    
+
+
+        if(i<10)    
+            i=i+1;
+            numbeerAdult.innerHTML=i
+    })
+
+
+    moinsValeurAdult.addEventListener("click",()=>{
+
+     if(i>0){
+        i--;
+        numbeerAdult.innerHTML=i
+
+     }else{
+        i=0;
+     }
+
+
+    })
+
+
+    //pas adult
+
+    plusValeurMoins.addEventListener("click",function(){
+    
+
+
+        if(i<10)    
+            i=i+1;
+            numbeerMoins.innerHTML=i
+    })
+
+
+    moinsValeurMoins.addEventListener("click",()=>{
+
+     if(i>0){
+        i--;
+        numbeerMoins.innerHTML=i
+
+     }else{
+        i=0;
+     }
+
+
+    })
+
+
+
+
+    let prixAdult=document.querySelector('#prixAdult');
+
+    console.log(prixAdult)
+
+
+
+
+    
+
+
+
+
+
+
  
+
+
 
