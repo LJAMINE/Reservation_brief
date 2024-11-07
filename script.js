962f2c8e3f7cb739db5f3bb5bbbe1de43b2be87b
LJAMINE <aminelmgrmj@gmai.com>
2024-11-07 16:22:42 +0100

    second

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function scrollPage(sectionId) {
+var formData={};
+
+function submitFunct(){
+
+    // var formData={};
+
+//     formData["fullName"]=document.getElementById("fullName").value;
+//     formData["email"]=document.getElementById("email").value;
+//     formData["etatDepart"]=document.getElementById("etatDepart").value;
+//     formData["etatArrive"]=document.getElementById("etatArrive").value;
+//     formData["date"]=document.getElementById("date").value;
+//     console.log(formData)
+
+// return formData;
+
+
+
+    }
+ 
+
+
+
+
+function scrollintrodPage(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({
         behavior: 'smooth',
@@ -7,3 +30,42 @@ function scrollPage(sectionId) {
 
      section.style.display = 'block';
 }
+
+
+function scrollPage(sectionId) {
+    let inputValue=document.getElementById("fullName").value;
+    let email=document.getElementById("email").value;
+    let etatDepart=document.getElementById("etatDepart").value;
+    let etatArrive=document.getElementById("etatArrive").value;
+    let date=document.getElementById("date").value;
+
+    if(inputValue && email && etatDepart && etatArrive && date){
+    
+       console.log(inputValue)   
+       console.log(email)   
+       console.log(etatDepart)   
+       console.log(etatArrive)   
+       console.log(date)   
+       
+       const section = document.getElementById(sectionId);
+       section.scrollIntoView({
+           behavior: 'smooth',
+           block: 'start'
+       });
+   
+        section.style.display = 'block';
+        
+        }else{
+            alert("entrer les info ");
+        }
+
+
+
+    
+       
+    }
+   
+ 
+
+ 
+
